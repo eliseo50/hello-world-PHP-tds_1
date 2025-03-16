3fd3fba1319130441ea25a4d2f20a620f2719d5e
eliseo50 <eliseo.gomez@galileo.edu>
2025-03-16 20:34:38 +0000

    initial commit: added a basic PHP page that displays "Hello World" with my name.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo PHP</title>
+</head>
+<body>
+    <h1><?php 
+    $nameStudent = "Eliseo Gomez";
+    echo "¡Hola Mundo! soy $nameStudent"; 
+    ?></h1>
+    <img src="./assets/image.webp" alt="">
+</body>
+</html>
